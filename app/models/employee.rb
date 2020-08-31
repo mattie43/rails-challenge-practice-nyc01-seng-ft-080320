dc65eca4b801d595fa2667862bb1c3f634f4ab40
Mattie43 <ericksenm93@ymail.com>
2020-08-30 20:35:40 -0400

    added minimum 5 characters for name

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,7 @@
 class Employee < ApplicationRecord
     belongs_to :company
     validates :name, :title, :company, presence: true
+    validates :name, length: { minimum: 5 }
 
     def delete_employee
         self.destroy
