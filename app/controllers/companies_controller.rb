0e8ac1b1fd8a321968b42a49435b3bd14b717472
Mattie43 <ericksenm93@ymail.com>
2020-08-30 20:55:02 -0400

    can't figure out how to add employee

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,6 +12,17 @@ class CompaniesController < ApplicationController
         @company = Company.create(company_params)
     end
 
+    def new_employee
+        @new_employee = Employee.new
+        render :new_employee
+    end
+
+    def create_employee
+        company = Company.find(params[:id])
+        @new_employee = Employee.create(params.require(:employee).permit(:name, :title, company_id: company))
+        redirect_to company_path(company)
+    end
+
     private
 
     def company_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :buildings
+  post "companies/new_employee"
   resources :companies
-  resources :employees, only: [:new, :update]
+  # resources :employees, only: [:new, :update]
   resources :offices
 end
